fd2c88db78433e52d4e69b5b764f1ddd65731860
Kevin Emerson <kevinaemerson@gmail.com>
2024-03-14 21:32:23 -0400

    ghpages custom domain homepage

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/steagles-dynasty/",
+  // base: "/steagles-dynasty/",
   plugins: [react()],
 })
