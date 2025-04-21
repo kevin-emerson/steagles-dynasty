ea575a47576ed61b05300dca832e47363b040d2e
Kevin Emerson <kevinaemerson@gmail.com>
2025-04-21 14:31:44 -0400

    Move projectin file to public folder to make available for download

--- src/components/ProjectionHub.jsx
+++ src/components/ProjectionHub.jsx
@@ -2,7 +2,7 @@ export default function ProjectionHub() {
     return (
         <div style={{marginBottom: '10em'}}>
             <p>Check out the official Steagles Dynasty League Projections for the upcoming season below!</p>
-            <a href="/src/files/2025_Projections.xlsx" download>Download</a>
+            <a href="/public/2025_Projections.xlsx" download>Download</a>
         </div>
     )
 }
