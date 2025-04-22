b36808c93a6bf80af0a6db689d8ce6339e983731
Kevin Emerson <kevinaemerson@gmail.com>
2025-04-22 12:38:43 -0400

    Revert homepage field, hardcode remote path for projections download

--- src/components/ProjectionHub.jsx
+++ src/components/ProjectionHub.jsx
@@ -2,7 +2,7 @@ export default function ProjectionHub() {
     return (
         <div style={{marginBottom: '10em'}}>
             <p>Check out the official Steagles Dynasty League Projections for the upcoming season below!</p>
-            <a href="../../public/2025_Projections.xlsx" download>Download</a>
+            <a href="https://www.steaglesdynastyleague.ca/public/2025_Projections.xlsx" download>Download</a>
         </div>
     )
 }
